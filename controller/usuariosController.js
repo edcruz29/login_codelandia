4740f52da07677c3a62f00692444a5e9003a6d0e
Eduardo <galvaocruz16@gmail.com>
2021-07-25 20:19:14 -0300

    criado view cadastro

---
+++ controller/usuariosController.js
@@ -0,0 +1,1 @@
+const usuariosModel = require('../model/usuariosModel')
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usuariosController = require('../controller/usuariosController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login')
+});
+router.get('/cadastro', (req,res,next)=>{
+  res.render('cadastro')
+})
+
+router.post("/login", function (req, res, next) {
+  const { email, senha} = req.body;
+
+  res.send(`${email}`).redirect('/');
 });
 
 module.exports = router;
